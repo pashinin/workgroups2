1c310e5f1d7a47a1d7068d889f47a9f10dadf872
Sergey Pashinin <sergey@pashinin.com>
2014-02-19 17:01:13 +0400

    Moved subr-compat.el, dflet.el to wg-compat.el
    Also a lot of function aliases were removed.
    Todo: replace them with cl-...

--- src/workgroups-compat.el
+++ src/workgroups-compat.el
@@ -8,44 +8,59 @@
 
 ;;; Code:
 
-;; Emacs 24.3+, use "cl-labels" instead of "labels"
+(require 'macroexp)
+
+(or (fboundp 'declare-function)
+    (defmacro declare-function (&rest args)
+      nil))
+
 (if (version< emacs-version "24.3")
-    (progn
-      (require 'cl)
-      (defalias 'wg-every 'every)
-      (defalias 'wg-mapcan 'mapcan)
-      (defalias 'wg-mapcar* 'mapcar*)
-      (defalias 'wg-copy-list 'copy-list)
-      (defalias 'wg-find 'find)
-      (defalias 'wg-gensym 'gensym)
-      (defalias 'wg-position 'position)
-      (defalias 'wg-reduce 'reduce)
-      (defalias 'wg-remove* 'remove*)
-      (defalias 'wg-remove-duplicates 'remove-duplicates)
-      (defalias 'wg-remove-if 'remove-if)
-      (defalias 'wg-remove-if-not 'remove-if-not)
-      (defalias 'wg-labels 'labels)
-      (defalias 'wg-some 'some)
-      (defalias 'wg-subsec 'subseq)
-      (defalias 'wg-union 'union))
-  (progn
-    (require 'cl-lib)
-    (defalias 'wg-every 'cl-every)
-    (defalias 'wg-mapcan 'cl-mapcan)
-    (defalias 'wg-mapcar* 'cl-mapcar)
-    (defalias 'wg-copy-list 'cl-copy-list)
-    (defalias 'wg-find 'cl-find)
-    (defalias 'wg-gensym 'cl-gensym)
-    (defalias 'wg-position 'cl-position)
-    (defalias 'wg-reduce 'cl-reduce)
-    (defalias 'wg-remove* 'cl-remove)
-    (defalias 'wg-remove-duplicates 'cl-remove-duplicates)
-    (defalias 'wg-remove-if 'cl-remove-if)
-    (defalias 'wg-remove-if-not 'cl-remove-if-not)
-    (defalias 'wg-labels 'cl-labels)
-    (defalias 'wg-some 'cl-some)
-    (defalias 'wg-subsec 'cl-subseq)
-    (defalias 'wg-union 'cl-union)))
+    ;; sure it doesn't exist, but it won't be called anyway...
+    (autoload 'cl--compiling-file "cl")
+  (declare-function cl--compiling-file "cl" t t))
+
+(if (version< emacs-version "24.3")
+    ;; before that version, flet was not marked as obsolete, so use it
+    (defalias 'dflet 'flet)
+
+  ;; This should really have some way to shadow 'byte-compile properties, etc.
+  (defmacro dflet (bindings &rest body)
+    "Make temporary overriding function definitions.
+This is an analogue of a dynamically scoped `let' that operates on the function
+cell of FUNCs rather than their value cell.
+If you want the Common-Lisp style of `flet', you should use `cl-flet'.
+The FORMs are evaluated with the specified function definitions in place,
+then the definitions are undone (the FUNCs go back to their previous
+definitions, or lack thereof).
+
+\(fn ((FUNC ARGLIST BODY...) ...) FORM...)"
+    (declare (indent 1) (debug cl-flet))
+    `(cl-letf ,(mapcar
+             (lambda (x)
+               (if (or (and (fboundp (car x))
+                            (eq (car-safe (symbol-function (car x))) 'macro))
+                       (cdr (assq (car x) macroexpand-all-environment)))
+                   (error "Use `labels', not `dflet', to rebind macro names"))
+               (let ((func `(cl-function
+                             (lambda ,(cadr x)
+                               (cl-block ,(car x) ,@(cddr x))))))
+                 (when (cl--compiling-file)
+                   ;; Bug#411.  It would be nice to fix this.
+                   (and (get (car x) 'byte-compile)
+                        (error "Byte-compiling a redefinition of `%s' \
+will not work - use `labels' instead" (symbol-name (car x))))
+                   ;; FIXME This affects the rest of the file, when it
+                   ;; should be restricted to the flet body.
+                   (and (boundp 'byte-compile-function-environment)
+                        (push (cons (car x) (eval func))
+                              byte-compile-function-environment)))
+                 (list `(symbol-function ',(car x)) func)))
+             bindings)
+       ,@body)))
+
+;;;###autoload
+(autoload 'dflet "dflet")
+
 
 
 (provide 'workgroups-compat)
