a22d54a98331fb4211b93aa61ff158b54d556e30
Sergey Pashinin <sergey@pashinin.com>
2014-02-20 21:23:21 +0400

    Added cl-lib as a requirement

--- src/workgroups2.el
+++ src/workgroups2.el
@@ -6,6 +6,7 @@
 ;; Author: Sergey Pashinin <sergey at pashinin dot com>
 ;; Keywords: session management window-configuration persistence
 ;; Homepage: https://github.com/pashinin/workgroups2
+;; Package-Requires: ((cl-lib "0.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
